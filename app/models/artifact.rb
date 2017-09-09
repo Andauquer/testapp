9c05e66b2d0a1bc5f7f2136a3b722b42c994b5bd
Andauquer <geba23@hotmail.com>
2017-09-09 00:21:06 +0000

    Create Artifacts

---
+++ app/models/artifact.rb
@@ -0,0 +1,18 @@
+class Artifact < ActiveRecord::Base
+  belongs_to :project
+  
+  validates_presence_of :name, :upload
+  validates_uniqueness_of :name
+  validate :uploaded_file_size
+  
+  attr_accessor :upload
+  
+  MAX_FILESIZE = 10.megabytes
+  
+  def uploaded_file_size
+    if upload
+      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+    end
+  end
+  
+end
